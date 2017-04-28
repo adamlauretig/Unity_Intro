8123f975b3a84386455813a985c9144edca9eda2
adamlauretig <lauretig.1@buckeyemail.osu.edu>
2017-04-28 16:34:19 -0400

    more writing

--- toy_function.R
+++ toy_function.R
@@ -6,16 +6,11 @@
 library(parallel)
 library(data.table)
 load("~/fake_data.rdata") # note that we're using short filepaths
-
 paste_number_letter <- function(i = NULL, letter_vector = NULL){
   data.table(paste0(letter_vector[i], "_", i))
 }
-
 length_of_letters <- length(fake_data)
-
 function_out <- mclapply(1:length_of_letters, paste_number_letter, 
   letter_vector = fake_data, mc.cores = 3)
-
 toy_dt <- rbindlist(function_out)
-
 save(toy_dt, file = "~/toy_dt.rdata")
